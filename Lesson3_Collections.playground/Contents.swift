3a3ae6601753cb835eccd8696d15a0060f07ae80
Vinh Thanh Vu <vtv@vinh.com>
2015-10-08 00:16:09 -0400

    L3: Collections > Three Types of Collections > Array can hold any type of object

--- Lesson3_Collections.playground/Contents.swift
+++ Lesson3_Collections.playground/Contents.swift
@@ -21,4 +21,12 @@ moreNumbers = [85.0, 90.0, 95.0]
 let differentNumbers = [97.5, 98.5, 99.0]
 
 // Array concatenation is super convenient in Swift.
-moreNumbers += differentNumbers
+let allNumbers = moreNumbers + differentNumbers
+
+
+
+// An array can hold any type of object. 
+// var ciruit = [livingRoomSwitch, kitchenSwitch, bathroomSwitch]
+
+
+
