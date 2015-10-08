a8d0016e0416750a4e4bb46261ee21c9a81613ce
Vinh Thanh Vu <vtv@vinh.com>
2015-10-07 23:47:58 -0400

    L3: Collections > Three Types of Collections > Initializing Arrays

---
+++ Lesson3_Collections.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Collections
+
+import UIKit
+import Foundation
+
+// Array - ordered list of items 
+
+// Dictionary - collection of key-value pairs 
+
+// Set - unordered list of distinct values
+
+// Initalizing Arrays 
+// The verbose way 
+var numbers = Array<Double>()
+
+// More often you will see ...
+var moreNumbers = [Double]()
+moreNumbers = [85.0, 90.0, 95.0]
+
+// Array literal syntax
+let differentNumbers = [97.5, 98.5, 99.0]
